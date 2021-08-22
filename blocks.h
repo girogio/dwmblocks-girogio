ba33bdaa4cec9fd8a97aca5ec803f772feade910
Girogio <giorgiogrigolo03@gmail.com>
2021-08-23 00:06:46 +0200

    italy v2.o

--- blocks.h
+++ blocks.h
@@ -6,10 +6,12 @@ static const Block blocks[] = {
 	//{ "",		"get_volume_pamixer",		0,					10 },
 	//{ "",		"disk_perc",				300,				0 },
 	//{ "",		"cpu_temp",					60,					0 },
-    { "",       "bat_level",                1,                  1},
-    { "",       "get_vol",                  1,                   1},
-    { "",		"calendar",					60000000,			15 },
-	{ "",		"local_time",				60,					0 },
+        //
+    { "",       "kb_lang",                      1,                 4},
+    { "",       "bat_level",                    1,                 5},
+    { "",       "get_vol",                      1,                 10},
+    { "",	"calendar",			60000000,          15},
+    { "",	"local_time",			60,	           0},
 };
 
 // Sets delimeter between status commands. NULL character ('\0') means no delimeter.
